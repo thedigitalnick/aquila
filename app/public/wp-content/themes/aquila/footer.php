88ac977c4baf1300d65deba6d709010ee54b0c94
Nicholas <ntravaglini@icloud.com>
2023-03-03 00:32:04 -0500

    header and footer

---
+++ app/public/wp-content/themes/aquila/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Footer Template
+ * 
+ * @package Aquila
+ */
+?>
+
+    <footer>
+        <p>Footer</p>
+    </footer>
+    
+    <?php wp_footer(); ?>
+</body>
+</html>
--- app/public/wp-content/themes/aquila/index.php
+++ app/public/wp-content/themes/aquila/index.php
@@ -4,16 +4,18 @@
  * 
  * @package Aquila
  */
+
+ get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aquila WordPress Theme</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+
+<h1>Hello World!</h1>
+
+
+
+
+
+<?php 
+get_footer();
+
