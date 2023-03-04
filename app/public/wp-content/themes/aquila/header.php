d1f63e585b33cdf0ceb40a6aa979f10d15a2c0a1
Nicholas <ntravaglini@icloud.com>
2023-03-04 16:38:33 -0500

    enqueue style and script

--- app/public/wp-content/themes/aquila/header.php
+++ app/public/wp-content/themes/aquila/header.php
@@ -14,7 +14,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+    
+    <?php 
+        if ( function_exists( 'wp_body_open' ) ) {
+            wp_body_open();
+        }
+    ?>
+
     <header>
         <p>Header</p>
     </header>
